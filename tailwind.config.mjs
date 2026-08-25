6109db14aed4ec7d1c93a03ee0c27d1a7e060fba
Victor Neyra <victorneyraars@gmail.com>
2026-08-25 00:39:45 +0000

    feat: landing page oficial baqueano natales con descarga apk y diseño premium

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
